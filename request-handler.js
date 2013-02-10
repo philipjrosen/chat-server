048c529d1559bbd050581339a839ef7669e2b44a
Catalyst Students <accounts@catalystclass.com>
2013-02-10 15:17:18 -0800

    Passes all basic server tests

--- request-handler.js
+++ request-handler.js
@@ -1,25 +1,28 @@
 var qs = require("querystring");
-
 var jsonStuff = [];
 exports.requests = function(request, response) {
   var headers = defaultCorsheaders;
   console.log("Serving request type " + request.method + " for url " + request.url);
   headers['Content-Type'] = "application/json";
-  if(request.method === "GET"){
-    response.writeHead(200, headers);
-    response.end(JSON.stringify(jsonStuff));
+  if(request.url === 'http://127.0.0.1:8080/classes/room1'){
+    if(request.method === "GET"){
+      response.writeHead(200, headers);
+      response.end(JSON.stringify(jsonStuff));
+    }
+    if (request.method === "POST"){
+      response.writeHead(302, headers);
+      request.on('data', function(stuff){
+        jsonStuff.push(qs.parse(stuff));
+      });
+      request.on('end',function(){
+        response.end('\n');
+      });
+    };
   }
-
-  if(request.method === "POST"){
-    response.writeHead(302, headers);
-    request.on('data', function(stuff){
-      jsonStuff.push(qs.parse(stuff));
-      console.log(jsonStuff);
-    });
-    request.on('end',function(){
-      response.end('\n');
-    });
-  };
+  else {
+    response.writeHead(404, headers);
+    response.end();
+  }
 };
 
 var defaultCorsheaders = {
