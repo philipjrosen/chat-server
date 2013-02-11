34e75b6f3dd518db1b44e08823f408e541ffbd25
Catalyst Students <accounts@catalystclass.com>
2013-02-11 11:27:14 -0800

    Refactor request handler

--- request-handler.js
+++ request-handler.js
@@ -1,33 +1,28 @@
-
-var defaultCorsheaders = {
+var qs = require("querystring");
+var jsonStuff = [];
+exports.requests = function(request, response) {
+  var headers= {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10 // Seconds.
-};
+  };
 
-var qs = require("querystring");
-var jsonStuff = [];
-exports.requests = function(request, response) {
-  var headers = defaultCorsheaders;
   console.log("Serving request type " + request.method + " for url " + request.url);
   headers['Content-Type'] = "application/json";
-  
+
   if(request.url === '/'){
-    console.log("in request.url");
+    response.writeHead(200, headers);
     if(request.method === "GET"){
-      response.writeHead(200, headers);
       response.end(JSON.stringify(jsonStuff));
     }
     else if (request.method === "POST"){
-      response.writeHead(200, headers);
       request.on('data', function(stuff){
         jsonStuff.push(JSON.parse(stuff));
       });
       response.end();
     }
     else if(request.method === 'OPTIONS'){
-      response.writeHead(200, headers);
       response.end();
     }
   }
